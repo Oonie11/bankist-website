c71b5797caca4dfdce02e26f7a7fa5c29ef7c868
Oonie11 <utkarshpatel002@gmail.com>
2022-10-23 20:06:32 -0400

    add sticky nav using intersection API

--- script.js
+++ script.js
@@ -149,6 +149,41 @@ nav.addEventListener("mouseout", handleHover.bind(1));
 //* Sticky Navigation
 //////////////////////////////////////////////////
 
+//! The Intersection Observer API
+// const obsCallBack = function (entries, observer) {
+//   entries.forEach((entry) => {
+//     console.log(entry);
+//   });
+// };
+
+// const obsOption = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallBack, obsOption);
+// observer.observe(section1);
+
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+// console.log(navHeight.height);
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+  const { isIntersecting } = entry;
+  if (!isIntersecting) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
 //! /////////////////////////////////////
 //! OLDER METHODS
 //! /////////////////////////////////////
